21cd86b1b7b06bfa0edd26756a1df05f91befda0
Dana Wyeth <danawyeth@gmail.com>
2022-10-13 22:14:36 +0800

    Added the cutest wyeth icon in the intro area. I made the icon for like 2-3 hours and my eyes hurt.

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import './styles/app.scss';
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch} from 'react-router-dom';
 import Header from './components/Header';
+import Intro from './components/Intro';
 
 
 function App() {
   return (
     <>
       <Header/>
+      <Intro/>
     </>
   );
 }
---
+++ src/components/Intro.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Intro = () => {
+    return (
+        <div className="intro">
+            <img className="wyeth" src="./wyeth.png" alt="" />
+        </div>
+    )
+}
+
+export default Intro
