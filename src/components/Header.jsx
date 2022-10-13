c5b26b6182c03577a01998a3b441eabadef76926
Dana Wyeth <danawyeth@gmail.com>
2022-10-13 11:24:34 +0800

    Header almost done just gonna add functionalities and some more design hover

---
+++ src/components/Header.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import { BrowserRouter as Link } from "react-router-dom";
+
+const Header = () => {
+  return (
+    <>
+      <header className="header">
+          <div className="container flex">
+              <h1 className="logo">Wyeth</h1>
+              <div className="nav">
+                  <ul>
+                      <li><Link to="/intro">intro</Link></li>
+                      <li><Link to="/portfolio">portfolio</Link></li>
+                      <li><Link to="/about-me">about me</Link></li>
+                  </ul>
+              </div>
+              <button type="button" class="btn">LET'S TALK</button>
+          </div>
+      </header>
+    </>
+  );
+};
+
+export default Header;
