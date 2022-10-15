1acc94423af23bff77cdbcbde278d341e5b3be03
Dana Wyeth <danawyeth@gmail.com>
2022-10-16 00:08:36 +0800

    Finished bubble hover. Fixing connect with me button. tbc tom. To do: blender files and bg. Also funtions. to finish all today hopefully. been doing intro page for 2 days now sucks.

--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -12,18 +12,24 @@ const Intro = () => {
   return (
     <>
       <div className="intro flex">
-      <div className="leftIntro">
+        <div className="leftIntro">
           <div className="flex">
             <div className="box1">
-            <p><strong>Web Developer</strong> (React/Angular)</p>
+              <p>
+                <strong>Web Developer</strong> (React/Angular)
+              </p>
               <img className="webBubble" src="laptopBubble.png" alt="" />
             </div>
             <div className="box2">
-            <p><strong>UI/UX Designer</strong></p>
+              <p>
+                <strong>UI/UX Designer</strong>
+              </p>
               <img className="uiBubble" src="uiBubble.png" alt="" />
             </div>
             <div className="box3">
-            <p><strong>3D Designer</strong></p>
+              <p>
+                <strong>3D Designer</strong>
+              </p>
               <img className="tdBubble" src="bubble.png" alt="" />
             </div>
           </div>
@@ -36,9 +42,27 @@ const Intro = () => {
                   <p className="hi">{data.intro}</p>
                   <h1 className="name">{data.name}</h1>
                   <p className="craft">{data.craft}</p>
-                  <button type="button" className="btn">
+                  <div className="connectWMeDiv">
                     connect with me
-                  </button>
+                  </div>
+                  <div className="socialIcons">
+                      <button
+                        type="button"
+                        className="btn fa-brands fa-linkedin"
+                      ></button>
+                      <button
+                        type="button"
+                        className="btn fa-brands fa-github"
+                      ></button>
+                      <button
+                        type="button"
+                        className="btn fa-regular fa-envelope"
+                      ></button>
+                      <button
+                        type="button"
+                        className="btn fab fa-instagram instagram"
+                      ></button>
+                    </div>
                   <div className="long-text">
                     <p>{data.desc}</p>
                   </div>
@@ -50,8 +74,6 @@ const Intro = () => {
           <img className="wyethPng" src="./wyeth.png" alt="" />
         </div>
       </div>
-      
-      
     </>
   );
 };
