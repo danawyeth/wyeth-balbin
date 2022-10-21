69d8272ebe94fb83ca0f7ccf374066adfa4c2859
Dana Wyeth <danawyeth@gmail.com>
2022-10-21 23:58:15 +0800

    Finished portfolio part except functionalities and modal. Will continue tomorrow morning. Then after that ill have tp design some more websites so that I can put somthing on my portfolio lololol

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import './styles/app.scss';
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route} from 'react-router-dom';
-import Header from './components/Header';
-import Intro from './components/pageContent/Intro';
-import HomePages from './components/Homepages/HomePages';
-import Portfolio from './components/pageContent/Portfolio'
+import "./styles/app.scss";
+import React, { Component } from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Header from "./components/Header";
+import Intro from "./components/pageContent/Intro";
+import HomePages from "./components/Homepages/HomePages";
+import Portfolio from "./components/pageContent/Portfolio";
 
-
-function App() {
-  return (
-    <>
-    <div className="main">
-      <Router>
-          <Routes>
-              <Route index element={<HomePages/>} />
-              
-          </Routes>
-      </Router>
-      </div>
-    </>
-  );
+class App extends Component {
+  render() {
+    return (
+      <React.Fragment>
+        <div className="main">
+          <Router>
+            <Routes>
+              <Route index element={<HomePages />} />
+            </Routes>
+          </Router>
+        </div>
+      </React.Fragment>
+    );
+  }
 }
 
 export default App;
--- src/components/pageContent/Portfolio.jsx
+++ src/components/pageContent/Portfolio.jsx
@@ -1,112 +1,108 @@
 import React from "react";
-//import Swiper from 'react-id-swiper';
 import { Swiper, SwiperSlide } from "swiper/react";
-//import { Swiper, SwiperSlide } from "swiper/react";
-import SwiperCore, { Pagination } from 'swiper';
+
 import "swiper/scss";
 import {
-  Navigation,
-  EffectFade,
-//  Pagination,
   EffectFlip,
   EffectCoverflow,
 } from "swiper";
-import "swiper/scss/navigation";
-import 'swiper/scss/pagination';
-import "swiper/scss/effect-fade";
+import "swiper/scss/pagination";
+import SwiperCore, { Pagination } from "swiper";
+import { useState } from "react";
+import Button from 'react-bootstrap/Button';
+import Modal from 'react-bootstrap/Modal';
 
-SwiperCore.use([Pagination])
+SwiperCore.use([Pagination]);
 
-const portfolio = () => {
-  //const swiper = new Swiper('.swiper', {
-  //    // Optional parameters
-  //    //direction: 'vertical',
-  //    loop: true,
 
-  //    // If we need pagination
-  //    pagination: {
-  //      el: '.swiper-pagination',
-  //    },
 
-  //    // Navigation arrows
-  //    navigation: {
-  //      nextEl: '.swiper-button-next',
-  //      prevEl: '.swiper-button-prev',
-  //    },
+export default function Portfolio() {
+  const params = {
+    pagination: {
+      //  dynamicBullets: true,
+      clickable: true,
+    },
+    speed: 500,
+    slidesPerView: 3,
+    spaceBetween: 40,
+    loop: true,
+    className: "mySwiper",
+    grabCursor: "true",
+  };
 
-  //    // And if we need scrollbar
-  //    scrollbar: {
-  //      el: '.swiper-scrollbar',
-  //    },
-  //  });
+  const data = {
+    webDev: [
+      { img: "imgTest.jpg" },
+      { img: "imgTest2.jpeg" },
+      { img: "imgTest.jpg" },
+      { img: "imgTest2.jpeg" },
+      { img: "imgTest.jpg" },
+      { img: "imgTest2.jpeg" },
+    ],
+    uixDesign: [
+      { img: "imgTest.jpg" },
+      { img: "imgTest.jpeg" },
+      { img: "imgTest.jpg" },
+      { img: "imgTest.jpeg" },
+      { img: "imgTest.jpg" },
+      { img: "imgTest.jpeg" },
+    ],
+    rdDesign: [
+      { img: "imgTest1.jpg" },
+      { img: "imgTest2.jpeg" },
+      { img: "imgTest1.jpg" },
+      { img: "imgTest2.jpeg" },
+      { img: "imgTest1.jpg" },
+      { img: "imgTest2.jpeg" },
+    ],
+  };
 
-  //let swiper = new Swiper('.mySwiper', {
-  //    navigation: {
-  //              nextEl: '.swiper-button-next',
-  //              prevEl: '.swiper-button-prev',
-  //            }
-  //});
-
-    const params = {
-        //modules: {["EffectFade", "Pagination", "EffectCoverflow"]},
-        //pagination: {
-        //  dynamicBullets: true,
-        //  clickable: true
-        //}, 
-        speed: 500,
-        //navigation: true,
-        //navigation: {
-        //    nextEl: ".swiper-button-next",
-        //    prevEl: ".swiper-button-prev"
-        //  },
-        slidesPerView: 3,
-        spaceBetween: 20,
-        loop: true,  
-        className: "mySwiper",
-        grabCursor: "true"
-    };
+  const [show, setShow] = useState(false);
+  const handleClose = () => setShow(false)
+  const handleShow = () => setShow(true)
     
-    
+
+  let swiperPort = (x) => {
+    return (
+      <>
+        <Swiper {...params}>
+          {x.map((data) => {
+            return (
+              <>
+                <SwiperSlide onClick={handleShow}>
+                  <img src={data.img} alt="" />
+                </SwiperSlide>
+              </>
+            );
+          })}
+        </Swiper>
+      </>
+    );
+  };
+
   return (
     <>
       <div id="portfolio" className="portfolio">
         <div className="portContainer">
           <h1>P O R T F O L I O</h1>
           <p>Web Development</p>
-
-          <button id="swiper-button-prev" className="swiper-button-prev"></button>
-          <button className="swiper-button-next"></button>
-          <Swiper {...params}
-          pagination={{
-                      dynamicBullets: true,
-                      clickable: true
-                    }}
-          >
-            <SwiperSlide>
-              <img src="imgTest.jpg" alt="" />
-            </SwiperSlide>
-            <SwiperSlide>
-              <img src="imgTest2.jpeg" alt="" />
-            </SwiperSlide>
-            <SwiperSlide>
-              <img src="imgTest.jpg" alt="" />
-            </SwiperSlide>
-            <SwiperSlide>
-              <img src="imgTest2.jpeg" alt="" />
-            </SwiperSlide>
-          </Swiper>
-          
-
-          <div className="flex">
-            <button className="btn">aaa</button>
-            <button className="btn">aaa</button>
-          </div>
-          <div className="flex"></div>
-          <div className="flex"></div>
+          <div className="webDev">{swiperPort(data.webDev)}</div>
+          <p>UI UX Design</p>
+          <div className="uixDesign">{swiperPort(data.uixDesign)}</div>
+          <p>3D Design</p>
+          <div className="rdDesign">{swiperPort(data.rdDesign)}</div>
         </div>
       </div>
+
+      <Modal show={show} onHide={handleClose}>
+        <Modal.Header closeButton>
+          <Modal.Title>Modal heading</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>Woohoo, you're reading this text in a modal!</Modal.Body>
+        <Modal.Footer>
+         
+        </Modal.Footer>
+      </Modal>
     </>
   );
 };
-
-export default portfolio;
