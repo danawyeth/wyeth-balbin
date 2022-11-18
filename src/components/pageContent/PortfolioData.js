c6218690ebf28568de63a193e236128e78c7e18b
danawyeth <danawyeth@proton.me>
2022-11-18 13:18:28 +0800

    moved images to src and edited format imaegs ..

--- src/components/pageContent/PortfolioData.js
+++ src/components/pageContent/PortfolioData.js
@@ -6,8 +6,8 @@ const data = {
         {
             id: 1,
             title: "Work Portfolio",
-            imgModal: ["./wyeth-balbin/assets/portfolio/ppCover.jpeg"],
-            img: "./wyeth-balbin/assets/portfolio/ppCover.jpeg",
+            imgModal: ["../wyeth-balbin/assets/portfolio/ppCover.jpeg"],
+            img: "../wyeth-balbin/assets/portfolio/ppCover.jpeg",
             role: "Frontend Developer  & UI Designer",
             about: "Wyeth's work portfolio showcasing her past projects. She created the website in a span of one week. The portfolio's design shows a lot about her aesthetic and an eye for art",
             link: "adfajdshakjshdkajshda.com",
@@ -17,8 +17,8 @@ const data = {
         {
             id: 2,
             title: "Baguio Visita 2.0",
-            imgModal: ["./wyeth-balbin/assets/baguio-visita/bv1.jpeg", "./wyeth-balbin/assets/baguio-visita/bv.jpeg", "./wyeth-balbin/assets/baguio-visita/bv2.jpeg"],
-            img: "./wyeth-balbin/assets/baguio-visita/bv1.jpeg",
+            imgModal: ["../wyeth-balbin/assets/baguio-visita/bv1.jpeg", "../wyeth-balbin/assets/baguio-visita/bv.jpeg", "..wyeth-balbin/assets/baguio-visita/bv2.jpeg"],
+            img: "../wyeth-balbin/assets/baguio-visita/bv1.jpeg",
             role: "Frontend Developer & UI Designer",
             about: "Built and designed web components for Baguio VIS.I.T.A - the Baguio VIS.I.T.A. (Visitor Information and Travel Assistant) is the online registration system for Baguio tourists and travelers. It has become an instrumental tool for the city’s cautious re-start of tourism activities and gradual re-opening of the local economy.",
             link: "https://visita.baguio.gov.ph/",
@@ -27,8 +27,8 @@ const data = {
         {
             id: 3,
             title: "SkinLense",
-            img: "./wyeth-balbin/assets/skinLense/skinlense.png",
-            imgModal: ["./wyeth-balbin/assets/skinLense/sl3.png", "./wyeth-balbin/assets/skinLense/sl1.png", "./wyeth-balbin/assets/skinLense/sl2.png"],
+            img: "../wyeth-balbin/assets/skinLense/skinlense.png",
+            imgModal: ["../wyeth-balbin/assets/skinLense/sl3.png", "../wyeth-balbin/assets/skinLense/sl1.png", "../wyeth-balbin/assets/skinLense/sl2.png"],
             role: "Thesis Leader",
             about: "A real-time skin disease detection app based on pre-trained weights on MobileNetV2 trained by DermNet and SkinSight dataset with 6 categories of common skin diseases namely hives, eczema, herpes, melanoma, corn, and acne with an accuracy and effectiveness of 96.68%.",
             link: "",
@@ -37,8 +37,8 @@ const data = {
         {
             id: 4,
             title: "ongoing",
-            img: "./wyeth-balbin/assets/tobeupdated.png",
-            imgModal: ["./wyeth-balbin/assets/tobeupdated.png"],
+            img: "../wyeth-balbin/assets/tobeupdated.png",
+            imgModal: ["../wyeth-balbin/assets/tobeupdated.png"],
             role: "designer/developer",
             about: "Projects are still cooking.",
             link: "",
